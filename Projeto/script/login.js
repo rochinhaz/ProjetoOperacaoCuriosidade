2b515a0d4cd7924ca41e0657a1f88002a29b23e9
Jeferson Rocha <jeferson_rocha57@hotmail.com>
2023-10-17 14:53:05 -0300

    css_pt4

---
+++ Projeto/script/login.js
@@ -0,0 +1,8 @@
+function validateFields(){
+    const email = document.getElementById("lemail").value;
+    if (!email){
+        window.alert('Digite um email valido')
+    }else{
+        
+    }
+}
