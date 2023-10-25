edacdb924117407bc79e6db346e7aa2c720e3a1c
Jeferson Rocha <jeferson_rocha57@hotmail.com>
2023-10-25 15:08:57 -0300

    js

--- Projeto/script/login.js
+++ Projeto/script/login.js
@@ -1,11 +1,22 @@
+
+function validarEmail (email) {
+    var emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+     return emailPattern.test(email); 
+  }
+
 function logar(){
 
     var email = document.getElementById('lemail').value;
     var senha = document.getElementById('lsenha').value;
-    
-    if (email != "" && senha != ""){
-        location.href = "index.html";
+    email =validarEmail(email);
+
+    if (email ==true){
+        if(senha.length >= 6 ){
+            location.href = "index.html";
+        }else{
+        alert('Colocar uma senha com 6 digitos ou mais');
+        }
     } else{
-        alert('Preencher os campos');
+        alert('Colocar um email valido');
     }
 }
--- Projeto/script/novoCadastro.js
+++ Projeto/script/novoCadastro.js
@@ -1,3 +1,4 @@
+
 function selecionarInput (event){
     const inputClicado = event.target;
 
@@ -8,14 +9,93 @@ function selecionarInput (event){
     inputClicado.addEventListener('click',removeSelecao, {once:true});
 }
 
+listar();
+
 function gravar(){
+    //debugger;
 
     var nome = document.getElementById('inome').value;
+    var email = document.getElementById('iemail').value;
+    var status = document.getElementById('iativo').value;
 
-    if (nome != ""){
+    var validNome = false;
+    var validEmail = false;
+
+    if (nome.length>2 && email.length>2){
+        validNome = true;
+        validEmail = true;
+    }else{
+        window.alert('Preencher os campos nome e email corretamente');
+    }
+
+    if(status == ""){
+        status = "Inativo";
+    }else{
+        status="Ativo"
+    }
+
+    if(validNome == true && validEmail == true){
+        let arrayCad = [];
+        if(localStorage.getItem('dados')!=null){
+            arrayCad=JSON.parse(localStorage.getItem('dados'));
+        }else{
+            arrayCad;
+        }
+
+        var info =
+            {
+                nome, email,status
+            };
+        
+        arrayCad.push(info);
+        var infoJson = JSON.stringify(arrayCad)
+        localStorage.setItem('dados', infoJson);
+
+        window.alert('Usuario cadastrado');
+        window.location.href ="cadastro.html";
+
+        listar();
+    }
+
+    function listar(){
+        
+        /*const arr = JSON.parse(localStorage.getItem('dados'));
+
+        if (arr != null) {
+            let tr = '';
+            arr.map(conteudo => {
+                tr += `
+			        <tr>
+					    <td>${conteudo.nome}</td>
+					    <td>${conteudo.email}</td>
+					    <td>${conteudo.status}</td>
+				    </tr>`
+
+
+        })
+            tBody.innerHTML = tr;
+        }*/
+        
+        for(var i in dados){
+            var cli =JSON.parse(dados[i]);
+            $(".tbUser tbody").append("<tr>");
+            $(".tbUser tbody").append("<td>"+cli.nome+"</td>");
+            $(".tbUser tbody").append("<td>"+cli.email+"</td>");
+            $(".tbUser tbody").append("<td>"+cli.status+"</td>");
+        }
+        }
+    }
+
+
+
+
+
+/*
+var nome = document.getElementById('inome').value;
+ if (nome != ""){
         location.href ="cadastro.html";
         alert('Usuario Cadastrado');
     } else{
         alert('Preencher o campo "nome" ');
     }
-}
+*/
